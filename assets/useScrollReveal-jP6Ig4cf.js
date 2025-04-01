894e492d8e1676472acdd6fb29234a4773c63339
Ryan Latimer <rlatimer96@harborps.org>
2025-04-01 09:40:17 -0400

    Updates

---
+++ assets/useScrollReveal-jP6Ig4cf.js
@@ -0,0 +1,2 @@
+import{r as l}from"./index-C7hB9mzN.js";const i=()=>{l.useEffect(()=>{const t=document.querySelectorAll(".reveal"),e=()=>{const r=window.innerHeight,n=150;t.forEach(o=>{o.getBoundingClientRect().top<r-n&&o.classList.add("active")})};return e(),window.addEventListener("scroll",e),()=>{window.removeEventListener("scroll",e)}},[])};export{i as u};
+//# sourceMappingURL=useScrollReveal-jP6Ig4cf.js.map
