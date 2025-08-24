ad07c414598fc924a654c91fe366faf3ade6f0d5
Gavin Moceri <chmcs@proton.me>
2025-08-23 21:21:51 -0400

    Add new assets and functionality for routing and scroll reveal
    - Introduced `react-router-hash-link` for smooth scrolling to hash links.
    - Added new assets including images and HTML files for 404 and other pages.
    - Implemented `useScrollReveal` hook to add reveal animations on scroll.

--- dist/assets/index-DtP9L_PU.js
+++ dist/assets/index-DtP9L_PU.js
@@ -1,4 +1,4 @@
-import{d as e,j as r}from"./main-BcJg8zaO.js";/**
+import{d as e,j as r}from"./main-BLQC47QU.js";/**
  * @license lucide-react v0.471.2 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/react-router-hash-link.esm-I3P9F7UD.js
+++ dist/assets/react-router-hash-link.esm-I3P9F7UD.js
@@ -1,4 +1,4 @@
-import{R as m,N as h,L as O}from"./main-BcJg8zaO.js";/*! *****************************************************************************
+import{R as m,N as h,L as O}from"./main-BLQC47QU.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
